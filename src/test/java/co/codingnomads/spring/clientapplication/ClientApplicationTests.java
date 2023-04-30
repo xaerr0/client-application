d08bcc8a96440eccfb2a6f0180db76ec5b1d7373
Danny Brown <xaerr00@gmail.com>
2023-04-29 20:10:30 -0700

    First Commit

---
+++ src/test/java/co/codingnomads/spring/clientapplication/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package co.codingnomads.spring.clientapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
